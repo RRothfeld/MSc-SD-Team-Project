813ac21c315ecfc634139bafc5f43ebcfb324fd7
Mickey Pashov <pashov.m@gmail.com>
2015-01-14 17:53:26 +0000

    Preliminary implementation of the Match class

--- src/Match.java
+++ src/Match.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Team Foxtrot
  * JavaBall Referees
@@ -17,5 +19,79 @@
  */
 
 public class Match {
-	// TODO Mickey
+    
+    public int week; // week when the match is held 1-52
+    public String area; // area where the match is held
+    public String level; // "Junior" or "Senior"
+    //public RefereeAssociation referees; // list of all suitable referees
+    public ArrayList<Referee> referees;
+    // public Association referees = new RefereeAssociation();
+    // ^ same principle as List ls = new ArrayList(); ?
+    /*
+     * Brownie points for
+     * + reusability
+     * + use of inheritance
+     */
+    
+    /**
+     * Instantiates a new Match (default constructor)
+     */
+    public Match() {
+	week = 0; //TODO sentinel value as a CONSTANT?
+	area = "";
+	level = "";
+	initReferees();
+    }
+    
+    /**
+     * Instantiates a new Match given a calendar week, 
+     * area the match is held in, level of the match
+     * @param week the number of the calendar match is held in (1-52)
+     * @param level of the match (Junior or Senior)
+     * @param area that the match is held in
+     */
+    public Match(int week, String level, String area) {
+	this.week = week;
+	this.level = level;
+	this.area = area;
+	initReferees();
+    }
+    
+    /** Initiate Referees Array */
+    public void initReferees () {
+	referees = new ArrayList();
+	//TODO should I store null values?
+    }
+    
+    /** */
+    public int getWeek() {
+	return week;
+    }
+    
+    public String getArea() {
+	return area;
+    }
+    
+    public String getLevel() {
+	return level;
+    }
+    
+    public String getReferees() {
+	for (Referee rf: referees) {
+	    //rf.getName();
+	    //rf.getAllocations();
+	    //TODO selection of 2 most suitable refs
+	}
+	return "";
+    }
+    
+    public String matchDetails() {
+	// this will print to MatchAllocs.txt
+	// could have toString method for match?
+	String ref1 = "Derek Riordan";
+	String ref2 = "Jane Gray";
+	String match = String.format("%d %s %s % %s %s",week, level, area, ref1, ref2);
+	return "";
+    }
+    
 }
