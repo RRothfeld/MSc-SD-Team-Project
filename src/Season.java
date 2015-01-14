65d995dac5959d9d87e3b91eb3551b688ec66985
Redfielder <raoul.rothfeld+github@gmail.com>
2015-01-14 15:05:27 +0000

    test2raoulhalllooo

--- src/Season.java
+++ src/Season.java
@@ -1,3 +1,17 @@
+/**
+ * Team Foxtrot
+ * JavaBall Referees
+ * Season class organising the matches of a season.
+ * <p>
+ * University of Glasgow
+ * MSc/PGDip Information Technology/Software Development
+ * Team Project 2014/15
+ *
+ * @author  Raoul Rothfeld [2164502r]
+ * @version 1.0
+ * @since   14-01-2015
+ */
+
 public class Season {
 
 }
