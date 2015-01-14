245b4647ba4d67d7d6532ae94295fbce2ff27f13
Redfielder <raoul.rothfeld+github@gmail.com>
2015-01-14 15:13:07 +0000

    Added Headers

--- src/JavaBall.java
+++ src/JavaBall.java
@@ -1,7 +1,21 @@
 /**
- * @author Team Foxtrot
+ * Team Foxtrot
+ * JavaBall Referees
+ * XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+ * <p>
+ * University of Glasgow
+ * MSc/PGDip Information Technology/Software Development
+ * Team Project 2014/15
+ *
+ * @author  Miroslav Pashov, 1005139P
+ * @author  Andrew Lowson, 0800685L
+ * @author  Marco Cook, 2152599C
+ * @author  Raoul Rothfeld, 2164502R
  * 
+ * @version 1.0
+ * @since   14-01-2015
  */
+
 public class JavaBall 
 {
 
--- src/Season.java
+++ src/Season.java
@@ -7,7 +7,10 @@
  * MSc/PGDip Information Technology/Software Development
  * Team Project 2014/15
  *
- * @author  Raoul Rothfeld, 2164502r
+ * @author  Miroslav Pashov, 1005139P
+ * @author  Andrew Lowson, 0800685L
+ * @author  Marco Cook, 2152599C
+ * @author  Raoul Rothfeld, 2164502R
  * 
  * @version 1.0
  * @since   14-01-2015
