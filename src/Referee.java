d3b7d55e491c2c31851b4c2334171ed63b09491a
Andrew Lowson <andrew@lowson.co>
2015-01-15 16:50:05 +0000

    90% of Referee Class done, 50% of RefAssoc
    Not Commented, that will be done tonight.

--- src/Referee.java
+++ src/Referee.java
@@ -1,3 +1,6 @@
+
+import java.io.FileReader;
+
 /**
  * Team Foxtrot
  * JavaBall Referees
@@ -24,6 +27,7 @@ public class Referee {
     //Basic information about Referee from the input file/user.
     private String forename;
     private String surname;
+    private String fullname; // Decide later
     private String uniqueID;
     private String qualification;
     private String homeLocality;
@@ -38,32 +42,7 @@ public class Referee {
     
     private boolean[] visitArea;
     
-    // char[] to store Y/N 3 times, one for each locality.
-    private char[] travelLocations;
-    
     private int matchesRefereed;
-    
-    //-------------------------------------------------------//
-    
-    /**
-     *
-     * @param id
-     * @param name
-     * @param qual
-     * @param home
-     * @param travel
-     */
-    public Referee(String id, String name, String qual,
-                    String home, String travel) 
-    {
-        setName(name);
-        setTravelLocations(travel);
-        
-             uniqueID = id;
-        qualification = qual;
-        homeLocality  = home;
-        
-    }
     
     //-------------------------------------------------------//
     
@@ -74,33 +53,121 @@ public class Referee {
     {
     
     }
+    /**
+     *
+     * @param id
+     * @param forename
+     * @param surname
+     * @param qualification
+     * @param matchesRefereed
+     * @param homeLocality
+     * @param travel
+     */
+    public Referee(String id, String forename, String surname, String qualification,
+                    String homeLocality, int matchesRefereed, String travel) 
+    {
+        String name = forename + " " + surname;
+        setTravelLocations(travel);
+             
+        this.uniqueID        = id;
+        this.forename        = forename;
+        this.surname         = surname;
+        this.fullname        = name;
+        this.qualification   = qualification;
+        this.homeLocality    = homeLocality;
+        this.matchesRefereed = matchesRefereed;
+                   
+    }
     
     //-------------------------------------------------------//
+    
+    /**
+     *
+     * @param fileLine
+     */
+        
+    public Referee(String fileLine)
+    {
+        String [] refereeDetails = fileLine.split(" ");
+        
+        if (refereeDetails != null && refereeDetails.length == 7)
+        {
+            this.uniqueID        = refereeDetails[0];
+            this.forename        = refereeDetails[1];
+            this.surname         = refereeDetails[2];
+            this.qualification   = refereeDetails[3];
+            this.matchesRefereed = Integer.parseInt(refereeDetails[4]);
+            this.homeLocality    = refereeDetails[5];
+            
+            setTravelLocations(refereeDetails[6]);
+        }
+    }
     
     private void setTravelLocations(String travel)
     {
         //remove any possible whitespace
         travel = travel.trim();
         
-        // If we go with char array
-        travelLocations = travel.toCharArray();
-        
         // if we go with boolean array.
         for (int i=0;i<travel.length();i++)
         {
             visitArea[i] = travel.charAt(i)=='Y';
         }   
+        
+        // Other implimentation
+        visitNorth  = visitArea[0];
+        visitCentre = visitArea[1];
+        visitSouth  = visitArea[2];
+        
     }
     
     //-------------------------------------------------------//
     
-    private void setName(String fullname)
+    /**
+     *
+     * @param ID
+     */
+        
+    public void setID(String ID)
+    {
+        this.uniqueID = ID;
+    }
+    //-------------------------------------------------------//
+    
+    /**
+     *
+     * @return
+     */
+    public String getID()
+    {
+        return uniqueID;
+    }
+    
+    //-------------------------------------------------------//
+
+    /**
+     *
+     * @param fullname
+     */
+        public void setName(String fullname)
     {
         String [] names = fullname.split(" ");
         
         forename = names[0];
         surname  = names[1];
+    }
+    
+    //-------------------------------------------------------//
+    
+    /**
+     *
+     * @param forename
+     * @param surname
+     */
         
+    public void setFullName(String forename, String surname)
+    {
+        this.fullname = forename+" "+surname;   
     }
     
     //-------------------------------------------------------//
@@ -111,8 +178,6 @@ public class Referee {
      */
     public String getFullName()
     {
-        String fullname = forename + " " + surname;
-        
         return fullname;
     }
     
@@ -120,22 +185,91 @@ public class Referee {
     
     /**
      *
-     * @param fn
+     * @param forename
      */
-    public void setForename(String fn)
+    public void setForename(String forename)
     {
-        forename = fn;
+        this.forename = forename;
     }
     
     //-------------------------------------------------------//
     
     /**
      *
-     * @param fn
+     * @return
      */
-    public void setSurname(String fn)
+        
+    public String getForename()
     {
-        forename = fn;
+        return forename;
+    }
+    
+    //-------------------------------------------------------//
+    /**
+     *
+     * @param surname
+     */
+    public void setSurname(String surname)
+    {
+        this.surname = surname;
+    }
+    
+    //-------------------------------------------------------//
+    
+    /**
+     *
+     * @return
+     */
+        
+    public String getSurname()
+    {
+        return surname;
+    }
+    
+    
+    //-------------------------------------------------------//
+
+    /**
+     *
+     * @param qualifications
+     */
+        public void setQualifications(String qualifications)
+    {
+        this.qualification = qualifications;
+    }
+    
+    //-------------------------------------------------------//
+    
+    /**
+     *
+     * @return
+     */
+        
+    public String getQualifications()
+    {
+        return qualification;
+    }
+    
+    //-------------------------------------------------------//
+    
+    /**
+     *
+     * @param location
+     */
+        
+    public void setHomeLocation(String location)
+    {
+        this.homeLocality = location;
+    }
+    //-------------------------------------------------------//
+    
+    /**
+     *
+     * @return
+     */
+    public String getHomeLocation()
+    {
+        return homeLocality;
     }
     
     //-------------------------------------------------------//
@@ -149,7 +283,7 @@ public class Referee {
         matchesRefereed = matches;
     }
     
-    //-------------------------------------------------------//
+//-------------------------------------------------------//
     
     /**
      *
@@ -163,7 +297,7 @@ public class Referee {
     //-------------------------------------------------------//
     
     /**
-     *
+     * 
      */
     public void increaseMatchesRefereed()
     {
@@ -171,5 +305,91 @@ public class Referee {
     }
     
     //-------------------------------------------------------//
+    
+    /**
+     *
+     * @param travel
+     */
+        
+    public void setTravelAreas(String travel)
+    {
+        setTravelLocations(travel);
+    }
+    
+    //-------------------------------------------------------//
+    
+    /**
+     *
+     * @return
+     */
+        
+    public boolean[] getTravelLocations()
+    {
+        return visitArea;
+    }
+    //-------------------------------------------------------//
+    
+    /**
+     *
+     * @param index
+     * @return
+     */
+    public boolean getTravelLocationAtIndex(int index)
+    {
+        return visitArea[index];
+    }
+    
+    //-------------------------------------------------------//
+    
+    /**
+     *
+     * @param location
+     * @return
+     */  
+    public boolean getTravelLocation(String location)
+    {
+        if (location.equals("North"))
+        {
+            return visitNorth;
+        } 
+        else if (location.equals("Centre")) 
+        {
+            return visitCentre;
+        } 
+        else 
+        {
+            return visitSouth;
+        }
+    }
+    
+    /**
+     * Method to change a particular travel 
+     * @param index
+     * @param willTravel
+     */
+        
+    public void changeLocationAtIndex(int index, boolean willTravel)
+    {
+        visitArea[index] = willTravel;
+    }
+    
+    //-------------------------------------------------------//
+    
+    /**
+     * Method to change all three locations in one step
+     * @param travel - is the three character string eg. "YYN"
+     * 
+     */        
+    public void updateTravelLocations(String travel)
+    {
+        //remove any possible whitespace
+        travel = travel.trim();
+        
+        // if we go with boolean array.
+        for (int i=0;i<travel.length();i++)
+        {
+            visitArea[i] = travel.charAt(i)=='Y';
+        }   
+    }
     
 }
