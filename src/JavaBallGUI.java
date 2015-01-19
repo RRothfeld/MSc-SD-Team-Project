3112e7619566d0f34fc5d7ec17a7d566b98f63b4
Mickey Pashov <pashov.m@gmail.com>
2015-01-19 22:21:14 +0000

    Added action listeners and a body for actionPerformed, changed the naming scheme of the components. Added displayChart method.

--- src/JavaBallGUI.java
+++ src/JavaBallGUI.java
@@ -7,95 +7,122 @@ import java.util.*;
 import java.io.*;
 
 /**
- * Team Foxtrot
- * JavaBall Referees - JavaBallGUI.java
- * Defines JavaBall GUI that displays referee and match details
+ * Team Foxtrot JavaBall Referees - JavaBallGUI.java Defines JavaBall GUI that
+ * displays referee and match details
  * <p>
- * University of Glasgow
- * MSc/PGDip Information Technology/Software Development
+ * University of Glasgow MSc/PGDip Information Technology/Software Development
  * Team Project 2014/15
  *
- * @author  Miroslav Pashov, 1005139P
- * @author  Andrew Lowson, 0800685L
- * @author  Marco Cook, 2152599C
- * @author  Raoul Rothfeld, 2164502R
+ * @author Miroslav Pashov, 1005139P
+ * @author Andrew Lowson, 0800685L
+ * @author Marco Cook, 2152599C
+ * @author Raoul Rothfeld, 2164502R
  * 
  * @version 1.0
- * @since   18-01-2015
+ * @since 18-01-2015
  */
 
 public class JavaBallGUI extends JFrame implements ActionListener {
-	// TODO Raoul
-	// DESIGN ACCORDING TO MARCO GUI FRAME #2
+    // TODO Raoul
+    // DESIGN ACCORDING TO MARCO GUI FRAME #2
+
+    // GUI components as instance variables
+    private JPanel navPanel, centrePanel, searchPanel, listPanel;
+    private JButton addButton, chartButton, allocateButton, exitButton,
+	    searchButton;
+    private JTextField searchField;
+    private JTextArea areaRefereeList;
+
+    //
+    private JFrame chart;
+    private RefereeAssociation referees;
+
+    /**
+     * Constructor for JavaBallGUI
+     */
+    public JavaBallGUI() {
+	// initiate GUI and its components
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	setTitle("JavaBall Referees");
+	setSize(800, 500);
+	layoutComponents();
+	updateRefereeList();
+    }
+
+    private void layoutComponents() {
+	// create JPanels
+	navPanel = new JPanel();
+	navPanel.setLayout(new BoxLayout(navPanel, BoxLayout.PAGE_AXIS));
+	centrePanel = new JPanel(new BorderLayout());
+	searchPanel = new JPanel();
+	listPanel = new JPanel();
+
+	// set JPanel backgrounds
+	navPanel.setBackground(Color.gray);
+	centrePanel.setBackground(Color.white);
+	searchPanel.setBackground(Color.white);
+	listPanel.setBackground(Color.white);
+
+	// add main JPanels to JFrame
+	add(navPanel, BorderLayout.WEST);
+	add(centrePanel, BorderLayout.CENTER);
+	centrePanel.add(searchPanel, BorderLayout.NORTH);
+	centrePanel.add(listPanel, BorderLayout.SOUTH);
+
+	// create JTextField
+	// length of JTextField measured in amount of visible 'm' characters
+	searchField = new JTextField(40); // length of 40 for String input
+	searchField.setText("Enter referee name or ID ...");
+
+	// create JButtons
+	addButton = new JButton("Add Referee");
+
+	chartButton = new JButton("Show Chart");
+	chartButton.addActionListener(this);
+
+	allocateButton = new JButton("Allocate Refs");
+	allocateButton.addActionListener(this);
 	
-	// GUI components as instance variables
-	private JPanel panelNavigation, panelCenter, panelSearch, panelList;
-	private JButton buttonAdd, buttonChart, buttonAllocate, buttonExit,
-			buttonSearch;
-	private JTextField fieldSearch;
-	private JTextArea areaRefereeList;
+	exitButton = new JButton("Save & Exit");
+	exitButton.addActionListener(this);
 	
-	/**
-	 * Constructor for JavaBallGUI
-	 */
-	public JavaBallGUI() {
-		// initiate GUI and its components
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setTitle("JavaBall Referees");
-		setSize(800, 500);
-		layoutComponents();
-		updateRefereeList();
+	searchButton = new JButton("Search");
+	searchButton.addActionListener(this);
+
+	// add center components to center panels
+	searchPanel.add(searchField);
+	searchPanel.add(searchButton);
+
+	// add navigation components to panelNavigation
+	navPanel.add(addButton);
+	navPanel.add(chartButton);
+	navPanel.add(allocateButton);
+	navPanel.add(exitButton);
+    }
+
+    private void updateRefereeList() {
+
+    }
+    
+    private void displayChart() {
+
+	chart = new ChartFrame(referees);
+	chart.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+	chart.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+	if (ae.getSource() == addButton) {
+	    System.out.println("Add");
+	} else if (ae.getSource() == allocateButton) {
+	   System.out.println("Allocate");
+	} else if (ae.getSource() == chartButton) {
+	    System.out.println("Chart");
+	} else if (ae.getSource() == searchButton) {
+	   System.out.println("Search");
+	} else if (ae.getSource() == exitButton) {
+	    System.exit(0);
 	}
 
-	private void layoutComponents() {
-		// create JPanels
-		panelNavigation = new JPanel();
-		panelNavigation.setLayout(new BoxLayout(panelNavigation,
-				BoxLayout.PAGE_AXIS));
-		panelCenter = new JPanel(new BorderLayout());
-		panelSearch = new JPanel();
-		panelList = new JPanel();
-
-		// set JPanel backgrounds
-		panelNavigation.setBackground(Color.gray);
-		panelCenter.setBackground(Color.white);
-		panelSearch.setBackground(Color.white);
-		panelList.setBackground(Color.white);
-			
-		// add main JPanels to JFrame
-		add(panelNavigation, BorderLayout.WEST);
-		add(panelCenter, BorderLayout.CENTER);
-		panelCenter.add(panelSearch, BorderLayout.NORTH);
-		panelCenter.add(panelList, BorderLayout.SOUTH);
-		
-		// create JTextField
-		// length of JTextField measured in amount of visible 'm' characters
-		fieldSearch = new JTextField(40); //length of 40 for String input
-		fieldSearch.setText("Enter referee name or ID ...");
-		
-		// create JButtons
-		buttonAdd = new JButton("Add Referee");
-		buttonChart = new JButton("Show Chart");
-		buttonAllocate = new JButton("Allocate Refs");
-		buttonExit = new JButton("Save & Exit");
-		buttonSearch = new JButton("Search");
-		
-		//add center components to center panels
-		panelSearch.add(fieldSearch);
-		panelSearch.add(buttonSearch);
-		
-		//add navigation components to panelNavigation
-		panelNavigation.add(buttonAdd);
-		panelNavigation.add(buttonChart);
-		panelNavigation.add(buttonAllocate);
-		panelNavigation.add(buttonExit);
-	}
-	
-	private void updateRefereeList() {
-		
-	}
-	
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-	}
+    }
 }
