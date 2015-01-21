c3aad812cc5a2243a2b72e09bf6961140309cf23
Andrew Lowson <andrew@lowson.co>
2015-01-21 20:14:24 +0000

    Added header preamble to Ref...it had disappeared

--- src/Referee.java
+++ src/Referee.java
@@ -1,5 +1,21 @@
 
 import java.util.ArrayList;
+/**
+ * Team Foxtrot - JavaBall Referees
+ * Aggregation class for all available and registered referees
+ * <p>
+ * University of Glasgow
+ * MSc/PGDip Information Technology/Software Development
+ * Team Project 2014/15
+ *
+ * @author  Miroslav Pashov, 1005139P
+ * @author  Andrew Lowson, 0800685L
+ * @author  Marco Cook, 2152599C
+ * @author  Raoul Rothfeld, 2164502R
+ * 
+ * @version 1.1
+ * @since   21-01-2015
+ */
 
 public class Referee implements Comparable<Referee> {
 
