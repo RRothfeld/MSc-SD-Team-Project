aa507fcb72c930f4ffee222dd54bebc642fee69a
Redfielder <raoul.rothfeld+github@gmail.com>
2015-02-12 13:41:54 +0000

    Revert Enum to NJB/IJB

--- src/RefQualification.java
+++ src/RefQualification.java
@@ -1,27 +1,15 @@
 /** The qualification of a referee */
 public enum RefQualification {
-    NJB ("National"), IJB ("International");
-    private String value;
-    public static int MAXIMUM = 4;
-    
-    private RefQualification(String value) {
-	this.value = value;
-    }
-    
-    @Override
-    public String toString() {
-	return value;
-    }
-    
-//    @Override
-//    public String toString() {
-//         switch (this) {
-//           case NJB:
-//                return  "National";
-//           case IJB:
-//                return "International";
-//          }
-//    return super.toString();
-//   }
+	NJB("NJB"), IJB("IJB");
+	private String value;
+	public static int MAXIMUM = 4;
 
+	private RefQualification(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return value;
+	}
 }
